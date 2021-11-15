ce7758fb530633e0021623b8b1131ab7b9af03a6
Franklin Waller <fr.l.waller@gmail.com>
2021-11-15 17:00:58 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import BN from "bn.js";
 
 export const API_URL = process.env.REACT_APP_API_URL || '';
 export const NULL_CONTRACT = 'null_address.near';
-export const MAX_GAS = new BN("100000000000000");
+export const MAX_GAS = new BN("250000000000000");
 export const DAO_ACCOUNT_ID = process.env.REACT_APP_DAO_ACCOUNT_ID || '';
 export const AUTH_ACCOUNT_ID = process.env.REACT_APP_AUTH_ACCOUNT_ID || '';
 export const STORAGE_BASE = new BN('30000000000000000000000');
